000ee95c24cace7fce9822fad3c45cf5479ea226
JorelAli <jorel.k.ali@gmail.com>
2020-03-30 19:48:13 +0100

    Improve syntax highlighting a bit

--- src/dev/jorel/fortelangprime/parser/exceptions/TypeException.java
+++ src/dev/jorel/fortelangprime/parser/exceptions/TypeException.java
@@ -2,6 +2,11 @@ package dev.jorel.fortelangprime.parser.exceptions;
 
 public class TypeException extends Exception {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public TypeException(String message) {
 		super(message);
 	}
