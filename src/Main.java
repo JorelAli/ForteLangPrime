e4fecafa1c3d6abd62907434f4e4661d3635d36b
JorelAli <jorel.k.ali@gmail.com>
2020-03-30 07:08:21 +0100

    Tidy up code; Add main method to gradle

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,14 @@ import java.io.File;
 import dev.jorel.fortelangprime.compiler.FLPCompiler;
 import dev.jorel.fortelangprime.parser.util.Pair;
 
-public class Testing {
+public class Main {
 	
 	public static void main(String[] args) throws Exception {
 		
-		new FLPCompiler(new File("test.flp"), new File("classfolder")).compile();
+		File[] files = new File(".").listFiles(f -> f.toString().endsWith("flp"));
+		for(File file : files) {
+			new FLPCompiler(file, new File("classfolder")).compile();
+		}
 		
 		System.out.println("Sample.id(true) = " + Sample.id(true));
 		System.out.println("Sample.id(\"hello\") = " + Sample.id("hello"));
@@ -19,16 +22,6 @@ public class Testing {
 		System.out.println(Sample.someOtherBool());
 		System.out.println(Sample.someOtherOtherBool());
 		
-//		System.out.println();
-//		Sample.Color color = Sample.red();
-//		System.out.println("red: " + color.red);
-//		System.out.println("green: " + color.green);
-//		System.out.println("blue: " + color.blue);
-//		
-//		color = Sample.green();
-//		System.out.println("red: " + color.red);
-//		System.out.println("green: " + color.green);
-//		System.out.println("blue: " + color.blue);
 	}
 	
 }
