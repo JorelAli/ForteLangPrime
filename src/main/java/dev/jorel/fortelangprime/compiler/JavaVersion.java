6006c4e06aca69c6b4e0b6eaf90347aa9163b4e1
Jorel Ali <contact@jorel.dev>
2020-07-13 17:33:31 +0100

    Convert gradle project into maven project

--- src/main/java/dev/jorel/fortelangprime/compiler/JavaVersion.java
+++ src/main/java/dev/jorel/fortelangprime/compiler/JavaVersion.java
@@ -5,7 +5,7 @@ import org.objectweb.asm.Opcodes;
 public enum JavaVersion implements Opcodes {
 
 	V_1(V1_1), V_2(V1_2), V_3(V1_3), V_4(V1_4), V_5(V1_5), V_6(V1_6), V_7(V1_7), V_8(V1_8), V_9(V9), V_10(V10),
-	V_11(V11), V_12(V12), V_13(V13), V_14(V14);
+	V_11(V11), V_12(V12);//, V_13(V13), V_14(V14);
 
 	private int version;
 
